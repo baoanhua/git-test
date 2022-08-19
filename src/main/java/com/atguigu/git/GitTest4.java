85381b7550e716b4c7920439e97196c9314dffdd
baoanhua <2993313597@qq.com>
2022-08-19 09:02:38 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest4.java
@@ -0,0 +1,7 @@
+package com.atguigu.git;
+
+public class GitTest4 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
---
+++ src/main/java/com/atguigu/git/GitTest5.java
@@ -0,0 +1,7 @@
+package com.atguigu.git;
+
+public class GitTest5 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
